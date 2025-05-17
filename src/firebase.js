ea044e5d244e3f77c8d2f242ced1052ac522f018
Conlin080110 <Conlin0801@gmail.com>
2025-05-17 16:26:13 +0900

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATIPDdUsSGEZI1tAItyEjOXh6GgZStbX8",
+  authDomain: "sarooom-4e732.firebaseapp.com",
+  projectId: "sarooom-4e732",
+  storageBucket: "sarooom-4e732.appspot.com",
+  messagingSenderId: "367375525181",
+  appId: "1:367375525181:web:6ca5ece64cfd39a11e9154"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider, signInWithPopup, signOut, doc, getDoc, setDoc };
